11e7d6aa716ca4505a06b5b0c9ad7e871f70dece
hikari24 <a.hikari24@gmail.com>
2020-05-09 11:52:24 +0000

    追加：users_controllerの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
-
 	protected
   	def configure_permitted_parameters
 	devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+	#新規登録時(sign_up)にnameのというキーのパラメータを追加で許可する
+	devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
+	#登録内容編集時(update)にnameのというキーのパラメータを追加で許可する
+
 	end
 
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+
+  protected
+
+  def update_resource(resource, params)
+      resource.update_without_current_password(params)
+  end
+
+  def after_update_path_for(resource)
+      users_path(resource)
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
