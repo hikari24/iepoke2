41a09cabbf89a6bc46bb0e263afd2f983f6d625f
hikari24 <a.hikari24@gmail.com>
2020-05-10 00:33:59 +0000

    修正：マイページの編集

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def update_resource(resource, params)
-      resource.update_without_current_password(params)
-  end
-
   def after_update_path_for(resource)
       users_path(resource)
   end
